3d018332fb4e835a0deef8771085a4a340683724
changangus <63685366+changangus@users.noreply.github.com>
2021-04-22 18:25:37 -0400

    update fridge form working, need to add to urql cache update list

--- src/components/MyFridgesItem.tsx
+++ src/components/MyFridgesItem.tsx
@@ -1,31 +1,35 @@
 import { Box, Button, Paper, Typography } from '@material-ui/core';
 import React from 'react';
-
+import NextLink from 'next/link'
 interface MyFridgesItemProps {
   title: string,
   imageUrl: string,
   id: string
 }
 
-const MyFridgesItem: React.FC<MyFridgesItemProps> = ({title, id, imageUrl}) => {
-    return (
-      <Box display="flex" flexDirection="column" alignItems="center">
-        <Paper elevation={3}>
-          <Box display="flex" justifyContent="center">
-            <Typography variant="h6">{title}</Typography>
+const MyFridgesItem: React.FC<MyFridgesItemProps> = ({ title, id, imageUrl }) => {
+  return (
+    <Box display="flex" flexDirection="column" alignItems="center">
+      <Paper elevation={3}>
+        <Box display="flex" justifyContent="center">
+          <Typography variant="h6">{title}</Typography>
+        </Box>
+        <Box display="flex" justifyContent="center" mb={3}>
+          <img
+            style={{ width: "275px", height: "230px", padding: "5px" }}
+            src={imageUrl} />
+        </Box>
+        <Box display="flex" justifyContent="space-evenly" padding={2}>
+          <Box display="flex" alignItems='center'>
+            <NextLink href={`/edit/${id}`}>
+              <Button variant="contained" color="secondary">Edit</Button>
+            </NextLink>
           </Box>
-          <Box display="flex" justifyContent="center" mb={3}>
-            <img 
-              style={{width: "275px", height: "230px", padding: "5px"}}
-              src={imageUrl}/>
-          </Box>
-          <Box display="flex" justifyContent="space-evenly" padding={2}>
-            <Button variant="contained">Edit</Button>
-            <Button variant="contained">Delete</Button>
-          </Box>
-        </Paper>
-      </Box>
-    );
+          <Button variant="contained">Delete</Button>
+        </Box>
+      </Paper>
+    </Box>
+  );
 }
 
 export default MyFridgesItem;
--- src/pages/edit/[fridgeId].tsx
+++ src/pages/edit/[fridgeId].tsx
@@ -2,16 +2,17 @@ import { Box, Typography, Button } from '@material-ui/core';
 import { Formik, Form, Field } from 'formik';
 import { TextField } from 'formik-material-ui';
 import { useRouter } from 'next/router';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useUpdateFridgeMutation, useGetFridgeQuery } from '../../generated/graphql';
 import ReactS3Client from 'react-aws-s3-typescript';
 import { s3Config } from '../../utils/s3Config';
-import { NextPage } from 'next';
 
-const UpdateFridgeForm: NextPage<{ fridgeId: string }> = ({ fridgeId }) => {
-  const [, updateFridge] = useUpdateFridgeMutation();
-  const [{data, fetching}] = useGetFridgeQuery({variables: {id: fridgeId}});
+
+const UpdateFridgeForm: React.FC = ({}) => {
   const router = useRouter();
+  const { fridgeId } = router.query;
+  const [, updateFridge] = useUpdateFridgeMutation();
+  const [{ data, fetching }] = useGetFridgeQuery({ variables: { id: fridgeId as string } });
   const [file, setFile] = useState(undefined as unknown as File);
 
   const uploadFile = async (): Promise<string> => {
@@ -21,118 +22,124 @@ const UpdateFridgeForm: NextPage<{ fridgeId: string }> = ({ fridgeId }) => {
     const url = res.location;
     return url;
   };
-
-  return (
-    <Box
-      height="90vh"
-      width="100%"
-      display="flex"
-      flexDirection="column"
-      justifyContent="space-evenly"
-      alignItems="center">
-      <Box mt={2} mb={2}>
-        <Typography variant="h4">New Fridge:</Typography>
-      </Box>
-      <Formik
-        initialValues={{
-          name,
-          address,
-          description,
-          instagram,
-          twitter
-        }}
-        onSubmit={async (values, { setFieldValue }) => {
-          if(!file) {
-            await updateFridge({ inputs: {...values, imageUrl: image, id} });
-          } else {
-            const res = await uploadFile(); 
-            await updateFridge({ inputs: {...values, imageUrl: res, id} });
-          }
-          router.push('/');
-        }}
-      >
-        {({ isSubmitting }) => (
-          <Form style={{ width: '70%' }}>
-            <Box
-              display="flex"
-              flexDirection="column"
-              justifyContent="space-around"
-              height="85%"
-              width="100%"
-              mt={6}
-            >
-              <Field
-                component={TextField}
-                id="name"
-                label="Fridge Name"
-                name="name"
-                variant="outlined"
-                required />
-              <Field
-                component={TextField}
-                id="address"
-                name="address"
-                label="Address"
-                variant="outlined" 
-                required />
-              <Field
-                component={TextField}
-                id="instagram"
-                name="instagram"
-                label="Instagram Link"
-                variant="outlined" />
-              <Field
-                component={TextField}
-                id="twitter"
-                name="twitter"
-                label="Twitter Link"
-                variant="outlined" />
-              <Field
-                component={TextField}
-                multiline
-                rows={6}
-                id="description"
-                name="description"
-                label="Description"
-                variant="outlined"
-                required
-              />
+  
+  if (!data?.getFridge) {
+    return (
+      <div>No Fridge Found</div>
+    )
+  } else {
+    return (
+      <Box
+        height="90vh"
+        width="100%"
+        display="flex"
+        flexDirection="column"
+        justifyContent="space-evenly"
+        alignItems="center">
+        <Box mt={2} mb={2}>
+          <Typography variant="h4">Update Fridge:</Typography>
+        </Box>
+        <Formik
+          initialValues={{
+            name: data.getFridge.name,
+            address: data.getFridge.address,
+            description: data.getFridge.description,
+            instagram: data.getFridge.instagram,
+            twitter: data.getFridge.twitter
+          }}
+          onSubmit={async (values, { setFieldValue }) => {
+            if (!file) {
+              await updateFridge({ inputs: { ...values, imageUrl: data.getFridge.imageUrl, id: data.getFridge._id as string} });
+            } else {
+              const res = await uploadFile();
+              await updateFridge({ inputs: { ...values, imageUrl: res, id: data.getFridge._id as string } });
+            }
+            router.push('/my-account');
+          }}
+        >
+          {({ isSubmitting }) => (
+            <Form style={{ width: '70%' }}>
               <Box
                 display="flex"
-                alignItems="center" 
-                mb={2}
-                mt={1}>
-                <Button
-                  variant="contained"
-                  component="label"
-                >
-                  Upload Image
+                flexDirection="column"
+                justifyContent="space-around"
+                height="85%"
+                width="100%"
+                mt={6}
+              >
+                <Field
+                  component={TextField}
+                  id="name"
+                  label="Fridge Name"
+                  name="name"
+                  variant="outlined"
+                  required />
+                <Field
+                  component={TextField}
+                  id="address"
+                  name="address"
+                  label="Address"
+                  variant="outlined"
+                  required />
+                <Field
+                  component={TextField}
+                  id="instagram"
+                  name="instagram"
+                  label="Instagram Link"
+                  variant="outlined" />
+                <Field
+                  component={TextField}
+                  id="twitter"
+                  name="twitter"
+                  label="Twitter Link"
+                  variant="outlined" />
+                <Field
+                  component={TextField}
+                  multiline
+                  rows={6}
+                  id="description"
+                  name="description"
+                  label="Description"
+                  variant="outlined"
+                  required
+                />
+                <Box
+                  display="flex"
+                  alignItems="center"
+                  mb={2}
+                  mt={1}>
+                  <Button
+                    variant="contained"
+                    component="label"
+                  >
+                    Upload Image
                   <input
-                    accept="image/*"
-                    type="file"
-                    id="file"
-                    onChange={(e: any) => {
-                      setFile(e.target.files[0]);
-                    }}
-                    hidden
-                  />
-                  
-                </Button>
-                <Typography style={{marginLeft: "10px"}} variant="body1">{file ? file.name : ''}</Typography>
-              </Box>
+                      accept="image/*"
+                      type="file"
+                      id="file"
+                      onChange={(e: any) => {
+                        setFile(e.target.files[0]);
+                      }}
+                      hidden
+                    />
 
-              <Button
-                color="primary"
-                variant="contained"
-                type="submit"
-                disabled={isSubmitting}
-              >Add New Fridge</Button>
-            </Box>
-          </Form>
-        )}
-      </Formik>
-    </Box>
-  );
+                  </Button>
+                  <Typography style={{ marginLeft: "10px" }} variant="body1">{file ? file.name : ''}</Typography>
+                </Box>
+
+                <Button
+                  color="primary"
+                  variant="contained"
+                  type="submit"
+                  disabled={isSubmitting}
+                >Update Fridge</Button>
+              </Box>
+            </Form>
+          )}
+        </Formik>
+      </Box>
+    )
+  };
 }
 
 export default UpdateFridgeForm;
